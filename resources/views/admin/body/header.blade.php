57536e2c7c1fdcc7fbecfd1fa7f6c83b676f3414
Hathappend <asepy183@gmail.com>
2024-03-03 17:02:32 +0700

    added logout fungsionality for admin

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -198,7 +198,7 @@
                     <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
                     <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <form action="{{ route('logout') }}" method="post">
+                    <form action="{{ route('admin.logout') }}" method="post">
                         <button class="dropdown-item text-danger" type="submit">
                             @csrf
                             <i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout
