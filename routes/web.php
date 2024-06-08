56002bad453408da16a14d21eebd74860b1f94bc
Hathappend <asepy183@gmail.com>
2024-06-08 12:23:44 +0700

    updated, admin home page slider

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Home\HomeSliderController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -27,11 +28,17 @@ Route::get('/dashboard', function () {
     return view('admin.index', ['user' => $findUser]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Admin Route
 Route::middleware('auth')->group(function (){
     Route::get('/admin/profile', [AdminProfileController::class, 'create'])->name('admin.profile');
     Route::get('/admin/profile/edit', [AdminProfileController::class, 'edit'])->name('admin.profile.edit');
     Route::post('/admin/profile/edit', [AdminProfileController::class, 'postEdit'])->name('admin.profile.postEdit');
     Route::post('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+});
+
+// Home Slide Route
+Route::controller(HomeSliderController::class)->group(function (){
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
 });
 
 Route::middleware('auth')->group(function () {
